658c94e28fa8b46abe544fc6cd08cb4e533d29e4
Dan Tolbert <dtolbert@bandwidth.com>
2019-03-15 12:42:11 -0400

    updates to text and stuff

---
+++ client/build/precache-manifest.7bceaf5cceb01c3558fbbdfb5e6307f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7be47164c1db896b630c",
+    "url": "/static/css/main.baff0a21.chunk.css"
+  },
+  {
+    "revision": "7be47164c1db896b630c",
+    "url": "/static/js/main.7be47164.chunk.js"
+  },
+  {
+    "revision": "a20e18f0acf2cca98576",
+    "url": "/static/js/1.a20e18f0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a812973b22bee120ea81278499e64634",
+    "url": "/index.html"
+  }
+];
